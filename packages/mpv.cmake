19de58e3ff2a0cf62663203b76819f030f8e30e3
mikkelone <mikkelone@localhost>
2026-02-23 17:04:37 +0100

    disable subrandr

--- packages/mpv.cmake
+++ packages/mpv.cmake
@@ -18,7 +18,6 @@ ExternalProject_Add(mpv
         libplacebo
         spirv-cross
         libsdl2
-        subrandr
     GIT_REPOSITORY https://github.com/mpv-player/mpv.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
@@ -49,7 +48,7 @@ ExternalProject_Add(mpv
         -Dspirv-cross=enabled
         -Dvulkan=enabled
         -Dvapoursynth=disabled
-        -Dsubrandr=enabled
+        -Dsubrandr=disabled
         ${mpv_gl}
         -Dc_args='-Wno-error=int-conversion'
     BUILD_COMMAND ${EXEC} LTO_JOB=1 PDB=1 ninja -C <BINARY_DIR>
