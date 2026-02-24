8a5057a479da8f787f6fb87749b0dc9c7976d6f9
mikkelone <mikkelone@localhost>
2026-02-24 05:20:11 +0100

    update gcc

--- toolchain/gcc/gcc.cmake
+++ toolchain/gcc/gcc.cmake
@@ -1,9 +1,9 @@
 ExternalProject_Add(gcc
     DEPENDS
         mingw-w64-headers
-    URL https://mirrorservice.org/sites/sourceware.org/pub/gcc/snapshots/14-20251205/gcc-14-20251205.tar.xz
+    URL https://mirrorservice.org/sites/sourceware.org/pub/gcc/snapshots/14-20260220/gcc-14-20260220.tar.xz
     # https://mirrorservice.org/sites/sourceware.org/pub/gcc/snapshots/13-20240309/sha512.sum
-    URL_HASH SHA512=b05c932de5c478cf9b64a4f2cbdf1de9b37a4845614ff8bc0e0563cd1e716f69ea0fe070ef9bf5ec1fccc0658705ec76cef536ac8dc4ca4c32cf81dad8588c37
+    URL_HASH SHA512=A9080DA674D31BB7B29685C49C4B0546CE8FE1A2BEC607F50A3A94EAD1AD4AC2015F0AE5510CE5CEB0C2AD089745CA0F55905BC36B087FE09F87C9471021E418
     DOWNLOAD_DIR ${SOURCE_LOCATION}
     CONFIGURE_COMMAND <SOURCE_DIR>/configure
         --target=${TARGET_ARCH}
